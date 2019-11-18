8cf816b98e3b3aa5b5fcd3bf6b7a2d7459ad3c4d
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-17 18:56:07 -0800

    Add method for fetching images

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Image from './Image';
 
 export class Images extends Component {
   // This is alternative class component syntax - class field declarations, allows you to initialize local state without using the constructor and declare class methods by using arrow functions without the extra need to bind them. See https://github.com/the-road-to-learn-react/react-alternative-class-component-syntax and https://www.robinwieruch.de/react-state-without-constructor
@@ -15,6 +16,17 @@ export class Images extends Component {
     axios
       .get(`/api/photos?count=${count}&start=${start}`)
       .then(res => this.setState({ images: res.data }));
+  }
+
+  fetchImages = () => {
+    const { count, start } = this.state;
+    // Will be at 1, then 31 (1 + 30), then 61 (31 + 30), and so forth
+    this.setState({start: this.state.start + count});
+    axios
+      .get(`/api/photos?count=${count}&start=${start}`)
+      .then(res =>
+        this.setState({ images: this.state.images.concat(res.data) })
+      );
   }
 
   render() {
