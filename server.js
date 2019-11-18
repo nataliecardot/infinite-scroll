cb028a2c4ed9fcf30da7a57b2ef6bb9f3b8d9a80
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-17 20:57:33 -0800

    Proxy fix

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.get('/api/photos', (req, res) => {
     .then(json => res.json(json));
 });
 
+if (process.env.NODE_ENV) {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
