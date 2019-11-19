2a5e5c712f42c73d12a7fa9d8d1d58dc328e90e9
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-18 21:09:27 -0800

    Link images

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -2,5 +2,7 @@ import React from 'react';
 
 // Extracting image from props object using destructuring, thus don't need to use props.image
 export default function Image({ image }) {
-  return <img className="single-photo" src={image.urls.regular} alt="" />
+  return <a href={image.links.html} target="_blank">
+    <img className="single-photo" src={image.urls.regular} alt="" />
+  </a>
 }
