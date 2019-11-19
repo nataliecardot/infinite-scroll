20cac833837a0fb83ef0f4375e09c6fe8740f8db
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-18 20:39:36 -0800

    Adjust styling

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 // Extracting image from props object using destructuring, thus don't need to use props.image
 export default function Image({ image }) {
