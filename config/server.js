ee08f4c40e666d033ec37163d6cfdadd98427aac
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-20 15:58:15 -0800

    Update config file

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  ACCESS_KEY: process.env.ACCESS_KEY || '3425b13734f847c0dd4496c5a9126c69a178f9c85639ea61f01e7e7afe41a0b5',
-  SECRET: process.env.SECRET || '6a3afcc1fa9647cb4c747c9b01d77e946b6e500f17d88fe7e6955c95641019ca',
+  ACCESS_KEY: process.env.ACCESS_KEY || 'Unsplash API key here',
+  SECRET: process.env.SECRET || 'Unsplash API secret here',
   CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000' // Running Node server on 5000, React on 3000
 };
