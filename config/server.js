1ed39ce2d0ac827ac5bb83f0c1b73a1445f328d7
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-20 21:11:27 -0800

    Remove config

--- config/server.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  ACCESS_KEY: process.env.ACCESS_KEY || '6c20bc304d082e6c68d2f583a8b72edd7ed075f4cdf4246b42c05b5b68929164',
-  SECRET: process.env.SECRET || 'c9888d7ac2342af452042f77592beaa7a3677c4fe43d812f96beb47811025bcd',
-  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000' // Running Node server on 5000, React on 3000
-};
