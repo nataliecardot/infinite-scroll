668f14443f947007d245b15eed8a1f1981a5cb9b
Natalie-Cardot <nataliecardot@gmail.com>
2019-11-20 20:44:59 -0800

    Add config

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+  ACCESS_KEY: process.env.ACCESS_KEY || '60aabdf2ccddb61a4c888fe81c050d8825639b0f5bb300e2bca0572e1698422a',
+  SECRET: process.env.SECRET || '7be897bcbc02728319444bbd0edeefa05b2a33e76f4b8552ddcd9021beda7266',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000' // Running Node server on 5000, React on 3000
+};
